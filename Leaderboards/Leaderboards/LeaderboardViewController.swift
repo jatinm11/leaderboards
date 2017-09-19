90886538dd9bf1f2610d56192f649cd5d8ff9477
Jatin Menghani <Jatin11@Jatins-MacBook-Pro.local>
2017-09-19 11:39:09 -0600

    Add leaderboard and player tableview storyboards

---
+++ Leaderboards/Leaderboards/LeaderboardViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LeaderboardViewController.swift
+//  Leaderboards
+//
+//  Created by Jatin Menghani on 19/09/17.
+//  Copyright © 2017 Jatin Menghani. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var leaderboardTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        leaderboardTableView.delegate = self
+        leaderboardTableView.dataSource = self
+    }
+    
+    
+    // MARK:- Table View Data Source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leaderboardCell", for: indexPath) as! LeaderboardTableViewCell
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
---
+++ Leaderboards/Leaderboards/PlayerTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerTableViewCell.swift
+//  Leaderboards
+//
+//  Created by Jatin Menghani on 19/09/17.
+//  Copyright © 2017 Jatin Menghani. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    // MARK:- Outlets
+    
+    @IBOutlet var playerImageView: UIImageView!
+    @IBOutlet var playerNameLabel: UILabel!
+    
+}
