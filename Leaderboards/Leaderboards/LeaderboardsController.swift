4db70331707899d8d11172a7e26f1f3b173e72e9
Jatin Menghani <Jatin11@Jatins-MacBook-Pro.local>
2017-09-20 09:34:33 -0600

    Add leaderboard controller

---
+++ Leaderboards/Leaderboards/LeaderboardsController.swift
@@ -0,0 +1,21 @@
+//
+//  LeaderboardsController.swift
+//  Leaderboards
+//
+//  Created by Jatin Menghani on 19/09/17.
+//  Copyright © 2017 Jatin Menghani. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class LeaderboardsController {
+    
+    static let shared = LeaderboardsController()
+    
+    func fetchMatches(completion: @escaping (_ success: Bool) -> Void = { _ in }) {
+     
+        
+    }
+    
+}
