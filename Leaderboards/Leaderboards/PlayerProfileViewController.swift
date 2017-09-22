d10690bcff11a0b4555824fe00d538c027336c00
reddy88 <reddymithun88@gmail.com>
2017-09-22 10:52:36 -0600

    Partial pending match functionality

--- Leaderboards/Leaderboards/PlayerProfileViewController.swift
+++ Leaderboards/Leaderboards/PlayerProfileViewController.swift
@@ -2,53 +2,117 @@
 //  PlayerProfileViewController.swift
 //  Leaderboards
 //
-//  Created by Jatin Menghani on 21/09/17.
+//  Created by Mithun Reddy on 9/21/17.
 //  Copyright © 2017 Jatin Menghani. All rights reserved.
 //
 
 import UIKit
 
 class PlayerProfileViewController: UIViewController {
-
-    var player = GameController.shared.playersBelongingToCurrentGame
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        playerNameLabel.text = player[0].username
-        playerImageView.image = player[0].photo
+    @IBOutlet weak var playerImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var pendingMatchesView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func backBarButtonItemTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        GameController.shared.fetchAllPlayersForCurrentGame { (success) in
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
+    @IBAction func segmentedControlSegmentChanged(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            profileView.alpha = 1
+            pendingMatchesView.alpha = 0
+        case 1:
+            profileView.alpha = 0
+            pendingMatchesView.alpha = 1
+        default:
+            break
         }
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        pendingMatchesView.alpha = 0
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
+        
+        PlayerController.shared.fetchCurrentPlayer { (success) in
+            if success {
+                DispatchQueue.main.async {
+                    self.playerImageView.image = PlayerController.shared.currentPlayer?.photo
+                    self.usernameLabel.text = PlayerController.shared.currentPlayer?.username
+                    MatchController.shared.fetchPendingMatchesForCurrentPlayer { (success) in
+                        if success {
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
     
-    @IBOutlet var tableView: UITableView!
-    @IBOutlet var playerNameLabel: UILabel!
-    @IBOutlet var playerImageView: UIImageView!
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
 }
 
+// MARK: - UITableViewDataSource, UITableViewDelegate
 
-extension PlayerProfileViewController: UITableViewDelegate, UITableViewDataSource {
+extension PlayerProfileViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return GameController.shared.playersBelongingToCurrentGame.count
+        return MatchController.shared.pendingMatches.count
     }
     
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath)
-        let player = GameController.shared.playersBelongingToCurrentGame[indexPath.row]
-        cell.textLabel?.text = player.username
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "pendingMatchCell", for: indexPath) as? PendingMatchTableViewCell else { return PendingMatchTableViewCell() }
+        cell.updateViewsWith(MatchController.shared.pendingMatches[indexPath.row])
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let confirmTableViewRowAction = UITableViewRowAction(style: .normal, title: "Confirm") { (_, indexPath) in
+            let verifiedMatch = MatchController.shared.verifyMatch(MatchController.shared.pendingMatches[indexPath.row])
+            MatchController.shared.updateMatch(verifiedMatch, completion: { (success) in
+                if success {
+                    DispatchQueue.main.async {
+                        MatchController.shared.clearPendingMatch(at: indexPath.row)
+                        tableView.deleteRows(at: [indexPath], with: .automatic)
+                    }
+                }
+            })
+        }
+        
+        let denyTableViewRowAction = UITableViewRowAction(style: .destructive, title: "Deny") { (_, indexPath) in
+            MatchController.shared.deletePendingMatch(at: indexPath.row, completion: { (success) in
+                if success {
+                    DispatchQueue.main.async {
+                        MatchController.shared.clearPendingMatch(at: indexPath.row)
+                        tableView.deleteRows(at: [indexPath], with: .automatic)
+                    }
+                }
+            })
+        }
+        
+        return [confirmTableViewRowAction, denyTableViewRowAction]
     }
     
 }
