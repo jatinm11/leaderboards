b6e8d00b5c873ac0aab2d9c724e3a027fd493d47
jonathan orellana <Jonathan@Jonnys-MaBook-Pro.local>
2017-09-25 14:23:31 -0600

    Add Players animation

--- Leaderboards/Leaderboards/PlayerViewController.swift
+++ Leaderboards/Leaderboards/PlayerViewController.swift
@@ -34,14 +34,15 @@ class PlayerViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
+        GameController.shared.playersBelongingToCurrentGame = []
         self.dismiss(animated: true, completion: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        GameController.shared.fetchAllPlayersForCurrentGame { (success) in
+        GameController.shared.fetchAllPlayersForCurrentGame { (_) in
             DispatchQueue.main.async {
-                self.playerTableView.reloadData()
+                self.animateTable()
             }
         }
     }
@@ -60,11 +61,43 @@ class PlayerViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.alpha = 0
         cell.backgroundColor = UIColor.clear
+        UIView.animate(withDuration: 1.0) {
+            cell.alpha = 1.0
+        }
+    }
+    
+    func animateTable() {
+        playerTableView.reloadData()
+        let cells = playerTableView.visibleCells
+        
+        let tableViewHeight = playerTableView.bounds.size.height
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+        }
+        
+        var delayCounter = 0
+        for cell in cells {
+            UIView.animate(withDuration: 1.0, delay: Double(delayCounter) * 0.05, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                cell.transform = CGAffineTransform.identity
+                }, completion: nil)
+            delayCounter += 1
+        }
     }
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
-
 }
+
+
+
+
+
+
+
+
+
+
