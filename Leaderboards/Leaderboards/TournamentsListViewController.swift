2076196f056da0d9ab7969c09eef2ae7d1686914
reddy88 <reddymithun88@gmail.com>
2017-09-26 09:53:05 -0600

    Partial implementation of tournaments.

---
+++ Leaderboards/Leaderboards/TournamentsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TournamentsListViewController.swift
+//  Leaderboards
+//
+//  Created by Mithun Reddy on 9/26/17.
+//  Copyright © 2017 Jatin Menghani. All rights reserved.
+//
+
+import UIKit
+
+class TournamentsListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
