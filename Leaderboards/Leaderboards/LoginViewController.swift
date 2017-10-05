b7971a58e11a4ffde0cb3c2d5290864b514dd38b
reddy88 <reddymithun88@gmail.com>
2017-10-05 09:58:00 -0600

    Fix join playspace and new game constraints for smaller screen sizes. Remove username label animation from login screen.

--- Leaderboards/Leaderboards/LoginViewController.swift
+++ Leaderboards/Leaderboards/LoginViewController.swift
@@ -8,24 +8,17 @@
 
 import UIKit
 
-class LoginViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+class LoginViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let colorProvider = BackgroundColorProvider()
     
     @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var createUsernameLabel: UILabel!
-    @IBOutlet weak var createUserNameCenterYConstraint: NSLayoutConstraint!
-    
-    var labelHasMoved = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        usernameTextField.delegate = self
         usernameTextField.attributedPlaceholder = NSAttributedString(string: "Enter Username",
                                                                      attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
-        
-        createUsernameLabel.alpha = 0.0
         
         let randomColor = colorProvider.randomColor()
         view.backgroundColor = randomColor
@@ -55,30 +48,6 @@ class LoginViewController: UIViewController, UIImagePickerControllerDelegate, UI
             let newUserSelectImageVC = segue.destination as? NewUserSelectImageViewController
             newUserSelectImageVC?.username = username
         }
-    }
-    
-    
-    
-    // MARK: - UITextFieldDelegate
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        if let text = textField.text, text.isEmpty {
-            if labelHasMoved == false {
-                labelHasMoved = true
-                UIView.animate(withDuration: 0.3) {
-                    self.createUserNameCenterYConstraint.constant = -30
-                    self.createUsernameLabel.alpha = 1.0
-                }
-            }
-        } else if let text = textField.text, text.characters.count == 1, string == "" {
-            labelHasMoved = false
-            UIView.animate(withDuration: 0.3) {
-                self.createUsernameLabel.frame = CGRect(x: self.usernameTextField.frame.origin.x, y: self.usernameTextField.frame.origin.y, width: self.usernameTextField.frame.width, height: self.usernameTextField.frame.height)
-                self.createUsernameLabel.alpha = 0.0
-            }
-        }
-        
-        return true
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
