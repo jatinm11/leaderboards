207e1cfccc4cce6adb636d66706ec2eb265ca963
reddy88 <reddymithun88@gmail.com>
2017-10-05 10:52:28 -0600

    Change share playspace password message slightly

--- Leaderboards/Leaderboards/GamesViewController.swift
+++ Leaderboards/Leaderboards/GamesViewController.swift
@@ -147,7 +147,7 @@ class GamesViewController: UIViewController {
     
     @objc func shareShowPasswordButtonTapped() {
         guard let playspacePassword = PlayspaceController.shared.currentPlayspace?.password, let playspaceName = PlayspaceController.shared.currentPlayspace?.name else { return }
-        let textToShare = "My playspace \(playspaceName)'s password is: \(playspacePassword)"
+        let textToShare = "\(playspaceName)'s password is: \(playspacePassword)"
         let share = [textToShare]
         let activityVC = UIActivityViewController(activityItems: share, applicationActivities: nil)
         self.present(activityVC, animated: true, completion: nil)
